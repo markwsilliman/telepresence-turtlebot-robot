4190ce10dd1623041d9443f87bfb889f92a3db5a
Mark Silliman <mwsilliman@gmail.com>
2015-03-10 14:30:59 -0700

    init

---
+++ telepresence.py
@@ -0,0 +1,36 @@
+# Create a Telepresence robot using TurtleBot2
+
+import rospy
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
+import json
+import urllib2
+import time #for sleep()
+
+class turtlebot_telep():
+	server_public_dns = 'http://ec2-54-200-33-28.us-west-2.compute.amazonaws.com'
+	
+	def __init__(self):
+		#initialize ros node
+		rospy.init_node('turtlebot_telep', anonymous=False)
+
+		#what to do if shut down (e.g. ctrl + C or failure)
+		rospy.on_shutdown(self.shutdown)
+		
+	def move(self):
+		rospy.loginfo("move")
+		
+	def shutdown(self):
+        rospy.loginfo("Stop")
+
+
+
+if __name__ == '__main__':
+	move_checks = 0
+	try:
+        telebot = turtlebot_telep()
+        #keep checking for deliver_coffee until we shutdown the script with ctrl + c
+		while(telebot.move() and not rospy.is_shutdown()):
+                move_checks = move_checks + 1
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Exception thrown")
